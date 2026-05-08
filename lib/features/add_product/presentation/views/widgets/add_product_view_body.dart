2a3f17ad1b65cf74f750193c6659203f3dbf6279
Mu Abdullah <mohamed553231@gmail.com>
2026-05-08 14:50:10 +0300

    feat: Fix some issues Integrate Bloc for Add Product functionality and implement custom Bloc observer

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub_dashboard/core/widgets/custom_button.dart';
 import 'package:fruit_hub_dashboard/core/widgets/custom_text_form_field.dart';
 import 'package:fruit_hub_dashboard/features/add_product/domain/add_product_input_entity.dart';
+import 'package:fruit_hub_dashboard/features/add_product/presentation/manager/cubit/add_product_cubit.dart';
 import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/image_field.dart';
 import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/is_featured_check_box.dart';
 
@@ -85,7 +87,7 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                     if (_formKey.currentState!.validate()) {
                       _formKey.currentState!.save();
 
-                      AddProductInputEntity(
+                      AddProductInputEntity input = AddProductInputEntity(
                         name: name,
                         code: code,
                         description: description,
@@ -93,6 +95,8 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                         image: image!,
                         isFeatured: isFeatured,
                       );
+
+                      context.read<AddProductCubit>().addProduct(input);
                     } else {
                       autovalidateMode = AutovalidateMode.always;
                       setState(() {});
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub_dashboard/core/helper_functions/on_generate_routes.dart';
+import 'package:fruit_hub_dashboard/core/services/custom_bloc_observer.dart';
+import 'package:fruit_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruit_hub_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:fruit_hub_dashboard/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+    Bloc.observer = CustomBlocObserver();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setupGetIt();
   runApp(const MainApp());
 }
 
