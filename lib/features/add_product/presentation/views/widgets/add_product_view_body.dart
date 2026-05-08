ff1170401b6a654a4638975c094d2a4ddb7a3af7
Mu Abdullah <mohamed553231@gmail.com>
2026-05-08 08:52:38 +0300

    feat: Create add product input entity

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub_dashboard/core/widgets/custom_button.dart';
 import 'package:fruit_hub_dashboard/core/widgets/custom_text_form_field.dart';
+import 'package:fruit_hub_dashboard/features/add_product/domain/add_product_input_entity.dart';
 import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/image_field.dart';
 import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/is_featured_check_box.dart';
 
@@ -83,6 +84,15 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
                   if (image != null) {
                     if (_formKey.currentState!.validate()) {
                       _formKey.currentState!.save();
+
+                      AddProductInputEntity(
+                        name: name,
+                        code: code,
+                        description: description,
+                        price: price,
+                        image: image!,
+                        isFeatured: isFeatured,
+                      );
                     } else {
                       autovalidateMode = AutovalidateMode.always;
                       setState(() {});
