ead4224dccda52b13d5b2db33db6f00bfdbd18d7
Mu Abdullah <mohamed553231@gmail.com>
2026-05-09 06:55:50 +0300

    feat: Integrate add product cubit with ui

--- lib/features/add_product/presentation/views/add_product_view.dart
+++ lib/features/add_product/presentation/views/add_product_view.dart
@@ -5,7 +5,7 @@ import 'package:fruit_hub_dashboard/core/repos/product_repo/product_repo.dart';
 import 'package:fruit_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruit_hub_dashboard/core/widgets/build_app_bar.dart';
 import 'package:fruit_hub_dashboard/features/add_product/presentation/manager/cubit/add_product_cubit.dart';
-import 'package:fruit_hub_dashboard/features/add_product/presentation/views/widgets/add_product_view_body.dart';
+import 'package:fruit_hub_dashboard/features/add_product/presentation/views/add_product_view_body_bloc_consumer.dart';
 
 class AddProductView extends StatelessWidget {
   const AddProductView({super.key});
@@ -16,10 +16,10 @@ class AddProductView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar('Add Product'),
-      body: BlocProvider( 
+      body: BlocProvider(
         create: (context) =>
             AddProductCubit(getIt<ImageRepo>(), getIt<ProductRepo>()),
-        child: SafeArea(child: AddProductViewBody()),
+        child: SafeArea(child: AddProductViewBodyBlocConsumer()),
       ),
     );
   }
