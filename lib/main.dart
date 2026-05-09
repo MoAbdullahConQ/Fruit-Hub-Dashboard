c4d1c062a3c7fc90485648337d595a087bd9b920
Mu Abdullah <mohamed553231@gmail.com>
2026-05-09 09:14:21 +0300

    feat: Configure supabase with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,19 @@ import 'package:fruit_hub_dashboard/core/services/custom_bloc_observer.dart';
 import 'package:fruit_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruit_hub_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
 import 'package:fruit_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-    Bloc.observer = CustomBlocObserver();
+  Bloc.observer = CustomBlocObserver();
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Supabase.initialize(
+    url: 'https://gbkwywpmglxtfqkvqmhq.supabase.co',
+    anonKey: 'sb_publishable_HyOPvNwzCEURGanf0EIN0Q_INKU0CDP',
+  );
+
   setupGetIt();
   runApp(const MainApp());
 }
